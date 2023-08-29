77c32ec46cf7ee39122ae5e8c108a8750bef3206
Sarok Dastkhosh <saruk.dastkhosh5186@gmail.com>
2023-08-29 14:14:04 +0330

    Upload app.js

---
+++ first-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import Navbar from './Navbar';
+import Burger from "./Burger"
+
+class App extends Component {
+    render() {
+        return (
+            <Burger />
+        );
+    }
+}
+
+export default App;
